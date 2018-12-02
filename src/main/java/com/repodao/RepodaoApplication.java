4d44cee5df793f23aac978cf2e6fe45c2f527417
learn <doroshevychv.learn@gmail.com>
2018-12-02 12:47:13 +0200

    Initial commit

---
+++ src/main/java/com/repodao/RepodaoApplication.java
@@ -0,0 +1,12 @@
+package com.repodao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepodaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepodaoApplication.class, args);
+    }
+}
---
+++ src/test/java/com/repodao/RepodaoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.repodao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RepodaoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
